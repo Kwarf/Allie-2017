29fee22aa5be4861e4e570c7877fef595d4a06df
Jimmy Bergström <thekwarf@gmail.com>
2017-03-15 00:22:34 +0100

    Add all message types

--- src/protocol/message_type.rs
+++ src/protocol/message_type.rs
@@ -3,6 +3,10 @@ use std::str::FromStr;
 #[derive(Debug, PartialEq)]
 pub enum MessageType {
     Welcome,
+    StateUpdate,
+    Dead,
+    EndOfRound,
+    StartOfRound,
 }
 
 #[derive(Debug)]
@@ -16,6 +20,10 @@ impl FromStr for MessageType {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "welcome" => Ok(MessageType::Welcome),
+            "stateupdate" => Ok(MessageType::StateUpdate),
+            "dead" => Ok(MessageType::Dead),
+            "endofround" => Ok(MessageType::EndOfRound),
+            "startofround" => Ok(MessageType::StartOfRound),
             _ => Err(MessageTypeError::UnknownType),
         }
     }
@@ -28,5 +36,25 @@ mod tests {
     #[test]
     fn can_convert_welcome_type() {
         assert_eq!(MessageType::Welcome, MessageType::from_str("welcome").unwrap());
+    }
+
+    #[test]
+    fn can_convert_stateupdate_type() {
+        assert_eq!(MessageType::StateUpdate, MessageType::from_str("stateupdate").unwrap());
+    }
+
+    #[test]
+    fn can_convert_dead_type() {
+        assert_eq!(MessageType::Dead, MessageType::from_str("dead").unwrap());
+    }
+
+    #[test]
+    fn can_convert_endofround_type() {
+        assert_eq!(MessageType::EndOfRound, MessageType::from_str("endofround").unwrap());
+    }
+
+    #[test]
+    fn can_convert_startofround_type() {
+        assert_eq!(MessageType::StartOfRound, MessageType::from_str("startofround").unwrap());
     }
 }
